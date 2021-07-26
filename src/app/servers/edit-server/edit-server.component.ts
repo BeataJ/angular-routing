da4a215901964477f3e44522bbfb735db51055ec
beataj <beata@emc2wave.com>
2021-07-25 18:12:32 -0700

    add change id

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -31,7 +31,14 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
         this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;      }
     );
     this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    // Subscribe route params to update the id if params change
+    // this.route.params.subscribe(
+    //   (params: Params) => {
+
+    //   }
+    // )
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -46,7 +53,7 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
     if(!this.allowEdit) {
       return true;
     }
-    if((this.serverName !== this.serverName || this.server.status !== this.server.status) && !this.changesSaved) {
+    if((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
       return confirm('Do you want to discard the changes?');
     } else {
       return true
