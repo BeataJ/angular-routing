8db62b936951229f2a143636d1aa2d8886d2a7f5
beataj <beata@emc2wave.com>
2021-07-26 16:05:32 -0700

    add Resolver, interface

--- src/app/servers/server/server-resolver.service.ts
+++ src/app/servers/server/server-resolver.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+interface Server { id: number, name: string, status: string }
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServerResolverService {
+export class ServerResolverService implements Resolve<Server>{
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | {
 
-  constructor() { }
+  }
+
+  // constructor() { }
 }
